09d8f26d181a8be2851f3f689153068588c7b56d
Manoj <manojmilu1@gmail.com>
2024-06-28 08:35:44 +0530

    changes

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,24 +1,24 @@
 import Image from "next/image";
-import phone from "../../../public/images/phone.png";
-import livechat from "../../../public/images/livechat.png";
+import RingerVolume from "../../../public/images/RingerVolume.png";
+import ChatMessage from "../../../public/images/ChatMessage.png";
 import location from "../../../public/images/location.png";
 import image from "../../../public/images/image.png";
 export default function Contact() {
   return (
     <>
-      <main className="container">
+      <main className="container container-fluid">
         <h1 className="py-5">Contact Us</h1>
         <div className="row">
-          <div className="col-lg-6 col-md-6 col-sm-6  mx-auto py-5">
+          <div className="col-lg-6 col-md-6 col-sm-6  m-auto p-auto">
             <h2 className="text-center">Send a Message</h2>
             <p className="text-center">
               lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsu lorem
               ipsum lorem ipsum lorem ipsum
             </p>
             <form>
-              <div class="row g-2">
-                <div class="col-6">
-                  <div class="p-3">
+              <div className="row g-2">
+                <div className="col-6">
+                  <div className="p-3">
                     <input
                       type="text"
                       placeholder="Name"
@@ -26,8 +26,8 @@ export default function Contact() {
                     />
                   </div>
                 </div>
-                <div class="col-6">
-                  <div class="p-3">
+                <div className="col-6">
+                  <div className="p-3">
                     <input
                       type="number"
                       placeholder="Phone no."
@@ -37,9 +37,9 @@ export default function Contact() {
                 </div>
               </div>
 
-              <div class="row g-2">
-                <div class="col-6">
-                  <div class="p-3">
+              <div className="row g-2">
+                <div className="col-6">
+                  <div className="p-3">
                     <input
                       type="email"
                       placeholder="Email"
@@ -47,8 +47,8 @@ export default function Contact() {
                     />
                   </div>
                 </div>
-                <div class="col-6">
-                  <div class="p-3 ">
+                <div className="col-6">
+                  <div className="p-3 ">
                     <select className="form-select border-dark rounded-0">
                       <option value="">Interested in</option>
                       <option value=""></option>
@@ -57,18 +57,18 @@ export default function Contact() {
                   </div>
                 </div>
               </div>
-              <div class="row g-5">
-                <div class="col-lg-8 col-md-8 col-sm-8 col-8">
-                  <div class="p-3 d-flexs">
+              <div className="row g-5">
+                <div className="col-lg-8 col-md-8 col-sm-8 col-8">
+                  <div className="p-3 d-flexs">
                     <textarea
                       placeholder="Message"
-                      class="form-control border-dark rounded-0"
+                      className="form-control border-dark rounded-0 py-6"
                       rows="5"
                     ></textarea>
                   </div>
                 </div>
-                <div class="col-lg-2 col-md-2 col-sm-2 col-2 position-relative ">
-                  <div class="p-3 position-absolute bottom-0 end-0">
+                <div className="col-lg-2 col-md-2 col-sm-2 col-2 position-relative ">
+                  <div className="p-3 position-absolute bottom-0 end-0">
                     <input
                       type="submit"
                       value="Submit"
@@ -80,27 +80,27 @@ export default function Contact() {
             </form>
           </div>
           <div className="col-lg-6 col-md-6 col-sm-6  mx-auto py-5">
-            <Image src={image} alt="image" className="img-fluid" />
+            <Image src={image} alt="image" className="header-img" />
           </div>
         </div>
-        <section className="mx-auto py-5">
-          <h2 className="text-center">More Ways To Connect</h2>
-          <p className="text-center">
-            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsu lorem
-            ipsum lorem ipsum lorem ipsum
-          </p>
-          <div class="d-flex justify-content-around">
+        <section style={{ backgroundColor: '#D9D9D9' }} className="mx-auto py-5">
+          <div className="text-center">
+          <h2>More Ways To Connect</h2>
+            <p>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsu lorem 
+            ipsum lorem ipsum lorem ipsum</p>
+          </div>
+          <div className="d-flex justify-content-around" style={{ paddingTop: '2.5rem' }}>
             <div>
-              <Image src={phone} alt="phone" className="icons" />
-              <div className="text-center">+91 6986969</div>
+              <Image src={RingerVolume} alt="RingerVolume" className="icons" />
+              <div className="text-center pt-2">+91 6986969</div>
             </div>
             <div>
-              <Image src={livechat} alt="livechat" className="icons" />
-              <div className="text-center">Live chat</div>
+              <Image src={ChatMessage} alt="livechat" className="icons" />
+              <div className="text-center pt-2">Live chat</div>
             </div>
             <div>
               <Image src={location} alt="your location" className="icons" />
-              <div className="text-center">your location</div>
+              <div className="text-center pt-2">your location</div>
             </div>
           </div>
         </section>
@@ -108,15 +108,16 @@ export default function Contact() {
           <div className=" col-lg-12 col-md-12 col-sm-12 col-12">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3740.196356743821!2d85.82827517595804!3d20.3747923099171!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a1909d464f2a4a3%3A0x1eca19fd1ea4946d!2sSJ%20Developers%20%26%20Housing%20Pvt.Limited!5e0!3m2!1sen!2sin!4v1719388502224!5m2!1sen!2sin"
-              width="600"
+              width="100%"
               height="450"
               allowFullScreen
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
+              className="my-5"
             ></iframe>
           </div>
         </section>
-        <footer>
+        <section>
           <div className="row d-flex justify-content-between align-items-center">
             <div className="col-lg-6 col-md-12 col-sm-12 col-12 mx-auto">
               <p>Frequently Asked Questions</p>
@@ -128,11 +129,33 @@ export default function Contact() {
                 ipsum lorem ipsum lorem ipsum
               </p>
               <ul>
-                <li>Lorem ipsum doremant si </li>
-                <li>Lorem ipsum doremant si </li>
-                <li>Lorem ipsum doremant si </li>
+                <li>Lorem ipsum doremant si <span>&#9679;</span></li>
+                <li>Lorem ipsum doremant si <span>&#9679;</span></li>
+                <li>Lorem ipsum doremant si <span>&#9679;</span></li>
               </ul>
             </div>
+          </div>
+        </section>
+        <footer className="mx-auto py-5 bg-dark text-white">
+          <div className="d-flex justify-content-around">
+              <div>Join Us Today 
+              For Your Dream Home</div>
+              <div>
+                <h5>Address</h5>
+                <p>Your address xyz street,  city,
+                pin no.</p>
+              </div>
+              <div>
+                <div>
+                    <h5>Phone no.</h5>
+                    <p>+91 7863489438</p>
+                    <p>+91 7863489438</p>
+                </div>
+                <div>
+                <h5>Email</h5>
+                    <p>Youremail@gmail.com</p>
+                </div>
+              </div>
           </div>
         </footer>
       </main>

62572a82d8a74811088396708fd6a06ad445c257
pankaj145-pb <pankaj.ab.net@gmail.com>
2024-07-04 19:57:06 +0530

    flipbook implemented

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,32 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Add a rule to handle SVG files
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgoConfig: {
+              // Configure SVGO options here if needed
+            },
+          },
+        },
+        {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/_next/',
+            name: 'static/media/[name].[hash].[ext]',
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,66 @@
-export default function Home(){
-    return(<>
-    <div className="container">
-        SJDevelopers
-    </div>
-    </>)
+'use client'
+import HTMLFlipBook from "react-pageflip";
+import Image from 'next/image'; // Import next/image for optimized image handling
+
+export default function Home() {
+    return (
+      <>
+        <div className="container container-fluid container-sm">
+            
+          <div className="justify-center">
+            <HTMLFlipBook width={500} height={500}>
+              <div className="demoPage">
+                {/* Use next/image component */}
+                <Image
+                  src="/images/amaze.jpg"
+                  alt="Page 1"
+                  width={500}
+                  height={500}
+                />
+              </div>
+              <div className="demoPage">
+              <Image
+                  src="/images/amaze1.jpg"
+                  alt="Page 1"
+                  width={500}
+                  height={500}
+                />
+              </div>
+              <div className="demoPage">
+              <Image
+                  src="/images/amaze2.jpg"
+                  alt="Page 1"
+                  width={500}
+                  height={500}
+                />
+              </div>
+              <div className="demoPage">
+              <Image
+                  src="/images/amaze3.jpg"
+                  alt="Page 1"
+                  width={500}
+                  height={500}
+                />
+              </div>
+              <div className="demoPage">
+              <Image
+                  src="/images/dummy.jpg"
+                  alt="Page 1"
+                  width={500}
+                  height={500}
+                />
+              </div>
+              <div className="demoPage">
+              <Image
+                  src="/images/dummy.jpg"
+                  alt="Page 1"
+                  width={500}
+                  height={500}
+                />
+              </div>
+            </HTMLFlipBook>
+          </div>
+        </div>
+      </>
+    );
 }
